8576906ecac30f36184d8010c7055ee8a6500f89
ViniciusMSoares <vinicius.soares@ccc.ufcg.edu.br>
2018-05-02 00:24:03 -0300

    config

---
+++ src/test/java/server/SeiSaudeApplicationTests.java
@@ -0,0 +1,14 @@
+package server;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SeiSaudeApplicationTests {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
