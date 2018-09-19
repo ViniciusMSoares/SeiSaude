3055daef7cd4ab36ba270e3664acf4dad652058c
DamiaoRobson <damiao.domiciano@ccc.ufcg.edu.br>
2018-09-19 06:25:10 -0300

    implementacao de consulta elemento, nao finalizada

---
+++ src/main/resources/public/js/controllers/consultaElemento.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var app;
+app = angular.module('Sei-Saude');
+
+app.controller('consultaCtrl', function($http, $window) {
+
+    var consulta = this;
+    consulta.elemento = {};
+
+    var url = 'https://sei-saude.herokuapp.com/';
+
+    consulta.elemento = function() {
+        
+        var nome = consulta.elemento.nome;
+
+        $http({
+            method: 'GET',
+            data: nome,
+            url: url + 'elemento'
+        }).then(function (success){
+            console.log({success});
+
+        },function (error){
+            console.log({error});
+        });
+    
+    
+
+    consulta.allElemento = function(){
+        $http({
+            method: 'GET',
+            data: nome,
+            url: url + 'all_elemento'
+        }).then(function (success){
+            console.log({success});
+        });
+    }
+}})
